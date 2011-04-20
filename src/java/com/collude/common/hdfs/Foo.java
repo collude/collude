e693667a0c19a6977baa38e787d529dfe64f2c79
florian <florian@twitter.com>
2011-04-19 21:41:43 -0700

    pants commit

---
+++ src/java/com/collude/common/hdfs/Foo.java
@@ -0,0 +1,17 @@
+package com.collude.common.hdfs;
+/**
+* Foo class
+* @author The Benjamins
+*/
+final class Foo {
+
+  private Foo() {
+
+  }
+  /**
+  * Main Method
+  **/
+  public static void main(String[] args) {
+    System.out.println("Welcome");
+  }
+}
